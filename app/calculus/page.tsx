d81265bf9c0d7b6268faab2cfa8d2a324bd2b8f9
30619 <21sunrin128@sunrint.hs.kr>
2023-04-26 15:11:45 +0900

    Update api

--- app/calculus/page.tsx
+++ app/calculus/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import integral from "@/app/api/backend/integral";
+
 export default function Calculus() {
+    const onIntegralClick = (e: any) => {
+       console.log(integral("sdfsf"));
+    }
     return (
         <div>
+            <button onClick={onIntegralClick}>click</button>
         </div>
     );
 }
